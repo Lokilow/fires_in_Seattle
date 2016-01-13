e97f7ceb19382dd0689ef4402c8f1e96d9af0e6b
Lokilow <cdlowen17@gmail.com>
2016-01-13 14:40:45 -0800

    cleaned up

---
+++ mapDict.py
@@ -0,0 +1,16 @@
+
+
+map_dict = {'Food On The Stove':"https://clowen.cartodb.com/viz/4bda1c9e-5bfa-11e5-b50f-0e9d821ea90d/embed_map",
+     'all fire torque':"https://clowen.cartodb.com/viz/b7b00480-5b44-11e5-a399-0e8dde98a187/embed_map",
+     'Illegal Burn':"https://clowen.cartodb.com/viz/c8597922-5bff-11e5-8edf-0e9d821ea90d/embed_map",
+     'Monorail Fire on the beam':"https://clowen.cartodb.com/viz/0e0403e4-5bff-11e5-97cc-0e853d047bba/embed_map",
+     'Dumpster Fire':"https://clowen.cartodb.com/viz/3aa5156e-5bff-11e5-956e-0e73ffd62169/embed_map",
+     'Rubbish Fire':"https://clowen.cartodb.com/viz/5dc4a924-5bff-11e5-a999-0ec6f7c8b2b9/embed_map",
+     'Rescue Lock In/Out':"https://clowen.cartodb.com/viz/339fb4bc-5c00-11e5-9e28-0e73ffd62169/embed_map",
+     'Shed Fire':"https://clowen.cartodb.com/viz/7c0240cc-5bff-11e5-90a7-0e018d66dc29/embed_map",
+     'Garage Fire':"https://clowen.cartodb.com/viz/9115472a-5bff-11e5-9641-0e0c41326911/embed_map",
+     'Chimney Fire':"https://clowen.cartodb.com/viz/a466d30c-5bff-11e5-a58e-0e9d821ea90d/embed_map",
+     'Brush Fire':"https://clowen.cartodb.com/viz/b82381ba-5bff-11e5-94c4-0e9d821ea90d/embed_map",
+     'Pier Fire':"https://clowen.cartodb.com/viz/e09308c8-5bff-11e5-9641-0e0c41326911/embed_map",
+     'Tunnel Fire':"https://clowen.cartodb.com/viz/fdc91e3c-5bff-11e5-97cc-0e853d047bba/embed_map",
+     'Transformer Fire':"https://clowen.cartodb.com/viz/13e6f5a4-5c00-11e5-a6f1-0e018d66dc29/embed_map"}
